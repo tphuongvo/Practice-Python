edae67020a81017b7990927109475cd0412978b3
tphuongvo <vnthphuong@gmail.com>
2022-09-02 12:34:39 +0700

    Test 1 -Coursera: algorithms-divide-conquer

--- karatsuba.py
+++ karatsuba.py
@@ -1,5 +1,8 @@
 """
-So: what's the product of the following two 64-digit numbers?
+Algorithms, Design and Analysis, Part I.
+Programming Assignment 1:
+
+What's the product of the following two 64-digit numbers?
 3141592653589793238462643383279502884197169399375105820974944592
 
 2718281828459045235360287471352662497757247093699959574966967627
