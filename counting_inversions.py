76613b082c3d90be803260ae96a0e752ab314828
tphuongvo <vnthphuong@gmail.com>
2022-09-02 13:00:35 +0700

    Test 2 - Coursera: Algorithms-Divide-and-Conquer

--- counting_inversions.py
+++ counting_inversions.py
@@ -9,46 +9,50 @@ Count inversions using Merge Sort.
 import math
 
 
-def merge_inversion(left, right):
-    res_arr, inv_count = [], 0
+def merge_count_inversion(left, right):
+    result_lst, inv_count = [], 0
     while len(left) > 0 or len(right) > 0:
         i, j = 0, 0
         if len(left) > 0 and len(right) > 0:
             if left[i] < right[j]:
-                res_arr.append(left[i])
+                result_lst.append(left[i])
                 i += 1
                 left = left[i:]
             else:
-                res_arr.append(right[j])
+                result_lst.append(right[j])
                 j += 1
                 right = right[j:]
                 inv_count += len(left)
         elif len(left) > 0:
-            res_arr.append(left[i])
+            result_lst.append(left[i])
             i += 1
             left = left[i:]
         elif len(right) > 0:
-            res_arr.append(right[j])
+            result_lst.append(right[j])
             j += 1
             right = right[j:]
 
-    return res_arr, inv_count
+    return result_lst, inv_count
 
 
-def merge_count_inversion(lst):
+def split(lst):
     size = len(lst)
     if len(lst) <= 1:
         return lst, 0
     mid = math.ceil(size // 2)  # no matter when choosing ceil or floor
-    left, a = merge_count_inversion(lst[:mid])
-    right, b = merge_count_inversion(lst[mid:])
-    result, c = merge_inversion(left, right)
+    left, a = split(lst[:mid])
+    right, b = split(lst[mid:])
+    result, c = merge_count_inversion(left, right)
     return result, (a + b + c)
+
+
+def merge_sort_inversion_counting(file):
+    with open(file, "r") as f:
+        results = split([int(i) for i in f])
+    return results[1]
 
 
 # list = [1, 3, 5, 2, 4, 6]
 # print(merge_count_inversion(list))
-with open("ItegerArray.txt", "r") as f:
-    results = merge_count_inversion([int(i) for i in f])
-
-print(results[1])  # 2407905288
+file = "ItegerArray.txt"
+print(merge_sort_inversion_counting(file))  # 2407905288
