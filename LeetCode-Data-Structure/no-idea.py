ec605d62369254a03858e693c5eb9c4e97af6433
tapivo <86455398+tphuongvo@users.noreply.github.com>
2024-02-12 15:54:36 +0700

    add hackkerank code

---
+++ LeetCode-Data-Structure/no-idea.py
@@ -0,0 +1,22 @@
+# There is an array of  integers. There are also  disjoint sets,  and , each containing  integers. You like all the integers in set  and dislike all the integers in set . Your initial happiness is . For each  integer in the array, if , you add  to your happiness. If , you add  to your happiness. Otherwise, your happiness does not change. Output your final happiness at the end.
+
+# Note: Since  and  are sets, they have no repeated elements. However, the array might contain duplicate elements.
+
+# Input Format
+
+# The first line contains integers  and  separated by a space.
+# The second line contains  integers, the elements of the array.
+# The third and fourth lines contain  integers,  and , respectively.
+
+# Output Format
+
+# Output a single integer, your total happiness.
+
+n, m = map(int, input().split())
+array = list(map(int, input().split()))
+A = set(map(int, input().split()))
+B = set(map(int, input().split()))
+
+happiness = sum(1 if element in A else -1 if element in B else 0 for element in array)    
+
+print(n,m, array, A, B, happiness)
