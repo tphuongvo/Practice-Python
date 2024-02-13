416524944a595f1f5ced8d8317611d34b56302f9
tapivo <86455398+tphuongvo@users.noreply.github.com>
2024-02-13 20:17:31 +0700

    update solution

--- LeetCode-Data-Structure/recursive-digit-sum.py
+++ LeetCode-Data-Structure/recursive-digit-sum.py
@@ -1,9 +1,15 @@
-import math
-import os
-import random
-import re
-import sys
+# We define super digit of an integer  using the following rules:
 
+# Given an integer, we need to find the super digit of the integer.
+
+# If  has only  digit, then its super digit is .
+# Otherwise, the super digit of  is equal to the super digit of the sum of the digits of .
+# For example, the super digit of  will be calculated as:
+
+# 	super_digit(9875)   9+8+7+5 = 29 
+# 	super_digit(29) 	2 + 9 = 11
+# 	super_digit(11)		1 + 1 = 2
+# 	super_digit(2)		= 2
 
 def addDigit(len_digit, n, const):
     
@@ -12,32 +18,42 @@ def addDigit(len_digit, n, const):
     return adding_zeros
 
 def numberAdding(n, k):
+
     number_temp = int(n)
     const = int(n)
 
     if (k == 1):
         sum_lst = list(map(int, str(number_temp)))
-        # print(sum_lst)
     elif (k > 1):
         for _ in range(k-1):
             number = addDigit(len(str(n)),number_temp, const)
             number_temp = number
-            # print(number_temp)
     
         sum_lst = list(map(int, str(number)))
     
     return sum(sum_lst)
 
 
-def superDigit(n, k):
+def superDigit_2(n, k):
 
     sum_lst = numberAdding(n,k)
-    # print(sum_lst)
+
     if len(str(sum_lst))==1:
         return sum_lst
     else:        
-        return superDigit(n=sum_lst,k=1)
-    
+        return superDigit_2(n=sum_lst,k=1)
+
+
+## Solution
+def superDigit(n, k):
+
+    sum_result = str(sum([int(number) for number in n])*k)
+
+    if len(sum_result) != 1:
+        return superDigit(sum_result,1)
+    else:
+        return sum_result
+
 
 first_multiple_input = input().rstrip().split()
 
@@ -45,5 +61,5 @@ n = first_multiple_input[0]
 
 k = int(first_multiple_input[1])
 
-result = superDigit(n, k)
-print(result)
+print(superDigit(n, k))
+print(superDigit_2(n, k))
